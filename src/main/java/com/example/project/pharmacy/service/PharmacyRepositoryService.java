0d051bf00c511a99de23f40eb2fd50be569acefc
yuyuhi <zaien24@naver.com>
2024-02-25 23:01:26 +0900

    10. Spring Transactional 사용시 주의사항

--- src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -15,6 +16,22 @@ import java.util.Objects;
 public class PharmacyRepositoryService {
 
     private final PharmacyRespostory pharmacyRespostory;
+
+    // self invocation test
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: " + TransactionalSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName : " + TransactionalSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRespostory.save(pharmacy);
+            throw new RuntimeException("error");  // 예외 발생
+        });
+    }
 
     @Transactional
     public void updateAddress(Long id, String address) {
