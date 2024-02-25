cb259c6c7d9598eb72bf6db4188a9a119a99aa46
yuyuhi <zaien24@naver.com>
2024-02-25 16:25:20 +0900

    06. kakao 주소검색 api 테스트 케이스 작성

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRespostoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRespostoryTest.groovy
@@ -12,6 +12,10 @@ class PharmacyRespostoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRespostory pharmacyRespostory;
 
+    def setup() {
+        pharmacyRespostory.deleteAll()
+    }
+
     def "PharmacyRepository save"() {
         given:
         String address = " 서울 특별시 성북구 종암동"
@@ -24,6 +28,7 @@ class PharmacyRespostoryTest extends AbstractIntegrationContainerBaseTest {
                 .pharmacyName(name)
                 .latitude(latitude)
                 .longitude(longitude)
+                .build()
 
         when:
         def result = pharmacyRespostory.save(pharmacy)
@@ -33,5 +38,27 @@ class PharmacyRespostoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = " 서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRespostory.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRespostory.findAll()
+
+        then:
+        result.size() == 1
     }
 }
