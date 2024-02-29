d79b25a2cbc013aa85e3313d0ce7fa676be0bcd8
yuyuhi <zaien24@naver.com>
2024-02-29 23:01:41 +0900

    01. 부트스트랩, jquery 및 Handlebars 라이브러리 적용

---
+++ src/main/java/com/example/project/direction/controller/FormController.java
@@ -0,0 +1,17 @@
+package com.example.project.direction.controller;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
