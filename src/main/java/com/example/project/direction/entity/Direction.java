2418a617832768ac8bc1586b188f1748a465b97e
한성길 <zaien24@naver.com>
2024-06-19 21:16:24 +0900

    Entity 주석 추가

--- src/main/java/com/example/project/direction/entity/Direction.java
+++ src/main/java/com/example/project/direction/entity/Direction.java
@@ -22,14 +22,17 @@ public class Direction extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 고객
     private String inputAddress;
     private double inputLatitude;
     private double inputLongitude;
 
+    // 약국
     private String targetPharmacyName;
     private String targetAddress;
     private double targetLatitude;
     private double targetLongitude;
 
+    // 고객 주소와 약국 주소 사이의 거리
     private double distance;
 }
